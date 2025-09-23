0321bd37d97c3805c182f5a21041b28766050423
papyrusilly <rebeca.bonifacio.silva@escola.pr.gov.br>
2025-09-23 11:53:07 -0300

    exercicio2

---
+++ pythonProject/cap 5/exercicio2.py
@@ -0,0 +1,10 @@
+def apresentaçao(nome, nomeMae, nomePai):
+    print(f"Nome: {nome}")
+    print(f"Nome da mae: {nomeMae}")
+    print(f"Nome do pai: {nomePai}")
+
+nome = str(input("Digite seu nome completo"))
+nomeMae = str(input("Digite o nome completo da sua mae"))
+nomePai = str(input("Digite o nome completo do seu pai"))
+
+apresentacao(nome, nomeMae, nomePai)
