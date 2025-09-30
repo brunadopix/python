b409c16aba8550b56478ab6053da850ca54a3be5
papyrusilly <rebeca.bonifacio.silva@escola.pr.gov.br>
2025-09-30 11:53:31 -0300

    exercicio4 e 5

---
+++ pythonProject/cap 5/exercicio4.py
@@ -0,0 +1,5 @@
+def validade(idade):
+    if (idade >= 18):
+        print("voce é maior de idade")
+idade = int(input("Digite a sua idade"))
+validade(idade)
---
+++ pythonProject/cap 5/exercicio5.py
@@ -0,0 +1,6 @@
+def resultado
+
+numero = int(input("Digite um número"))
+resultado(numeroAoQuadrado)
+imprima(numeroPositivo)
+print ("imprimir resultado positivo")
